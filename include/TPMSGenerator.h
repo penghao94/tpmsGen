80066f21cc9f095b01b7a850a2d510a089f84970
swannyPeng <ph1994wh@gmail.com>
2019-08-28 14:00:19 +0800

    upgrade Eigen version from 3.2.10->3.3.7

--- include/TPMSGenerator.h
+++ include/TPMSGenerator.h
@@ -15,7 +15,7 @@ namespace tpmsgen {
 			const DerivedSize&,
 			const DerivedSize&,
 			const DerivedSize&)> func;			//TPMS function
-		std::vector<DerivedSize>Vmin,Vmax;	// Bounding box
+		std::vector<DerivedSize>Vmin, Vmax;	// Bounding box
 		size_t solution;			// Resolution for each TPMS cell
 
 		TPMSGenerator(const std::function<DerivedValue(
@@ -27,27 +27,39 @@ namespace tpmsgen {
 			const size_t s) {
 			func = std::move(f); Vmin = m; Vmax = M; solution = s;
 		};
-		template<typename DerivedG,typename DerivedV>
+		template<typename DerivedG, typename DerivedV>
 		void getDistanceField(Eigen::PlainObjectBase<DerivedG> &grid, Eigen::PlainObjectBase<DerivedV> &value);
-		
-		template<typename DerivedVec,typename DerivedFct>
+
+		template<typename DerivedG, typename DerivedV>
+		void getDistanceField(const size_t label, Eigen::PlainObjectBase<DerivedG> &grid, Eigen::PlainObjectBase<DerivedV> &value);
+
+		template<typename DerivedVec, typename DerivedFct>
 		void makeLevelSet(const DerivedValue &level, Eigen::PlainObjectBase<DerivedVec> &vertices, Eigen::PlainObjectBase<DerivedFct>&facets);
+
+		template<typename DerivedVec, typename DerivedFct>
+		void makeLevelSet(const size_t label, const DerivedValue &level, Eigen::PlainObjectBase<DerivedVec> &vertices, Eigen::PlainObjectBase<DerivedFct>&facets);
+		template<typename DerivedVec, typename DerivedFct>
+		void makeLevelSet(const size_t label, const DerivedValue &level, Eigen::PlainObjectBase<DerivedVec> &vertices, Eigen::PlainObjectBase<DerivedFct>&facets, DerivedValue& density);
 
 		~TPMSGenerator() {};
 	};
-	
-	
+
+
 
 	template<typename DerivedValue, typename DerivedSize>
 	template<typename DerivedG, typename DerivedV>
 	inline void TPMSGenerator<DerivedValue, DerivedSize>::getDistanceField(Eigen::PlainObjectBase <DerivedG>& grid, Eigen::PlainObjectBase <DerivedV>& value)
 	{
+
+
+
+
 		std::vector<size_t> res(3);
 		for (int i = 0; i < 3; i++) res[i] = static_cast<size_t>((Vmax[i] - Vmin[i]) / PI * solution);
 
-		//std::cout << res[0] << res[1] << res[2] << std::endl;
-		grid.resize(res[0] * res[1] * res[2], 3);
-		value.resize(res[0] * res[1] * res[2], 1);
+		
+		grid.resize(res_0 * res[1] * res[2], 3);
+		value.resize(res_0 * res[1] * res[2], 1);
 
 		const auto lerp = [&](const size_t index, const size_t dim)->DerivedSize {
 			return Vmin[dim] + static_cast<DerivedSize>(index) / static_cast<DerivedSize>(res[dim] - 1)
@@ -58,11 +70,68 @@ namespace tpmsgen {
 			const DerivedSize pz = lerp(z, 2);
 			for (size_t y = 0; y < res[1]; y++) {
 				const DerivedSize py = lerp(y, 1);
-				for (size_t x = 0; x < res[0]; x++) {
+				for (size_t x = 0; x < res_0; x++) {
 					const DerivedSize px = lerp(x, 0);
 
-					grid.row(x + res[0] * (y + res[1] * z)) << px, py, pz;
-					value.row(x + res[0] * (y + res[1] * z)) << func(px, py, pz);
+					grid.row(x + res_0 * (y + res[1] * z)) << px, py, pz;
+					value.row(x + res_0 * (y + res[1] * z)) << func(px, py, pz);
+
+				}
+			}
+		}
+	}
+
+	// | 0/1 0/1 0/1 0/1 0/1 0/1|
+	// | xf  xb  yf  yb  zf  zb |
+
+
+	template<typename DerivedValue, typename DerivedSize>
+	template<typename DerivedG, typename DerivedV>
+	inline void TPMSGenerator<DerivedValue, DerivedSize>::getDistanceField(const size_t label, Eigen::PlainObjectBase<DerivedG>& grid, Eigen::PlainObjectBase<DerivedV>& value)
+	{
+		std::vector<size_t> res(3);
+		for (int i = 0; i < 3; i++) res[i] = static_cast<size_t>((Vmax[i] - Vmin[i]) / PI * solution);
+		
+		std::vector<int> bound(6);
+		for (size_t i = 0; i < 6; i++)
+			bound[i] = (label >> i) & 0x01;
+
+
+		int res_0 = bound[0] + res[0] + bound[2];
+		int res_1 = bound[1] + res[1] + bound[3];
+		int res_2 = bound[4] + res[2] + bound[5];
+
+		grid.resize(res_0 * res_1 * res_2, 3);
+		value.resize(res_0 * res_1 * res_2, 1);
+		const auto lerp = [&](const DerivedSize index, const size_t dim)->DerivedSize {
+			return Vmin[dim] + index / static_cast<DerivedSize>(res[dim] - 1)
+				*(Vmax[dim] - Vmin[dim]);
+		};
+		for (size_t z = 0; z < res_2; z++) {
+			const DerivedSize pz = lerp(static_cast<DerivedSize>(z)-bound[4], 2);
+
+			for (size_t y = 0; y < res_1; y++) {
+				const DerivedSize py = lerp(static_cast<DerivedSize>(y)-bound[1], 1);
+
+				for (size_t x = 0; x < res_0; x++) {
+					const DerivedSize px = lerp(static_cast<DerivedSize>(x)-bound[0], 0);
+
+					grid.row(x + res_0 * (y + res_1 * z)) << px, py, pz;
+					value.row(x + res_0 * (y + res_1 * z)) << func(px, py, pz);
+					
+					/*if (x == 0 && (label & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (x == res_0 - 1 && ((label >> 2) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == 0 && ((label >> 1) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == res_1 - 1 && ((label >> 3) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == 0 && ((label >> 4) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == res_2 - 1 && ((label >> 5) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+*/
 				}
 			}
 		}
@@ -81,7 +150,108 @@ namespace tpmsgen {
 		std::vector<size_t> res(3);
 		for (int i = 0; i < 3; i++) res[i] = static_cast<size_t>((Vmax[i] - Vmin[i]) / PI * solution);
 
-		tpmsgen::marching_cubes(value, grid, res[0], res[1], res[2],0.0, vertices, facets);
+		tpmsgen::marching_cubes(value, grid, res_0, res[1], res[2],0.0, vertices, facets);
+	}
+
+	template<typename DerivedValue, typename DerivedSize>
+	template<typename DerivedVec, typename DerivedFct>
+	inline void TPMSGenerator<DerivedValue, DerivedSize>::makeLevelSet(const size_t label, const DerivedValue & level, Eigen::PlainObjectBase<DerivedVec>& vertices, Eigen::PlainObjectBase<DerivedFct>& facets)
+	{
+		Eigen::Matrix<DerivedSize, Eigen::Dynamic, 3> grid;
+		Eigen::Matrix<DerivedValue, Eigen::Dynamic, 1> value;
+
+		getDistanceField(label,grid, value);
+		for (int i = 0; i < value.rows(); i++) value(i) -= level;
+		std::vector<size_t> res(3);
+		for (int i = 0; i < 3; i++) res[i] = static_cast<size_t>((Vmax[i] - Vmin[i]) / PI * solution);
+
+
+		std::vector<size_t> bound(6);
+		for (size_t i = 0; i < 6; i++)
+			bound[i] = (label >> i) & 0x01;
+
+		int res_0 = bound[0] + res[0] + bound[2];
+		int res_1 = bound[1] + res[1] + bound[3];
+		int res_2 = bound[4] + res[2] + bound[5];
+	
+		for (size_t z = 0; z < res_2; z++) {
+			for (size_t y = 0; y < res_1; y++) {
+				for (size_t x = 0; x < res_0; x++) {
+
+					if (x == 0 && (label & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (x == res_0 - 1 && ((label >> 2) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == 0 && ((label >> 1) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == res_1 - 1 && ((label >> 3) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == 0 && ((label >> 4) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == res_2 - 1 && ((label >> 5) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+
+				}
+			}
+		}
+
+
+		tpmsgen::marching_cubes(value, grid, res_0, res_1, res_2, 0.0, vertices, facets);
+		facets.col(1).swap(facets.col(2));
+	}
+
+	template<typename DerivedValue, typename DerivedSize>
+	template<typename DerivedVec, typename DerivedFct>
+	inline void TPMSGenerator<DerivedValue, DerivedSize>::makeLevelSet(const size_t label, const DerivedValue & level, Eigen::PlainObjectBase<DerivedVec>& vertices, Eigen::PlainObjectBase<DerivedFct>& facets, DerivedValue& density)
+	{
+		Eigen::Matrix<DerivedSize, Eigen::Dynamic, 3> grid;
+		Eigen::Matrix<DerivedValue, Eigen::Dynamic, 1> value;
+
+		getDistanceField(label, grid, value);
+		for (int i = 0; i < value.rows(); i++) value(i) -= level;
+
+		DerivedValue solid = 0;
+
+		for (int i = 0; i < value.rows(); i++)
+			if (value(i, 0) >= 0) solid += 1;
+		density = solid / value.rows();
+
+
+
+		std::vector<size_t> res(3);
+		for (int i = 0; i < 3; i++) res[i] = static_cast<size_t>((Vmax[i] - Vmin[i]) / PI * solution);
+
+
+		std::vector<size_t> bound(6);
+		for (size_t i = 0; i < 6; i++)
+			bound[i] = (label >> i) & 0x01;
+
+		int res_0 = bound[0] + res[0] + bound[2];
+		int res_1 = bound[1] + res[1] + bound[3];
+		int res_2 = bound[4] + res[2] + bound[5];
+
+		for (size_t z = 0; z < res_2; z++) {
+			for (size_t y = 0; y < res_1; y++) {
+				for (size_t x = 0; x < res_0; x++) {
+
+					if (x == 0 && (label & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (x == res_0 - 1 && ((label >> 2) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == 0 && ((label >> 1) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (y == res_1 - 1 && ((label >> 3) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == 0 && ((label >> 4) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+					else if (z == res_2 - 1 && ((label >> 5) & 0x01))
+						value(x + res_0 * (y + res_1 * z), 0) = value(x + res_0 * (y + res_1 * z), 0) > 0 ? 0 : value(x + res_0 * (y + res_1 * z), 0);
+
+				}
+			}
+		}
+		tpmsgen::marching_cubes(value, grid, res_0, res_1, res_2, 0.0, vertices, facets);
+		facets.col(1).swap(facets.col(2));
 	}
 
 }
--- test.cpp
+++ test.cpp
@@ -27,21 +27,60 @@ bool writeOBJ(
 		(F.array() + 1).format(IOFormat(FullPrecision, DontAlignCols, " ", "\n", "f ", "", "", "\n"));
 	return true;
 }
+bool writeLOG(const std::string & str, const Eigen::MatrixXd & param, int index=0)
+{
+	using namespace std;
+	using namespace Eigen;
+
+	ofstream csv;
+
+	if (index == 0)
+		csv.open(str);
+	else
+		csv.open(str, ios::app);
+
+	if (!csv.is_open())
+	{
+		fprintf(stderr, "IOError: writeCSV() could not open %s\n", str.c_str());
+		return false;
+	}
+
+	csv << param.format(IOFormat(FullPrecision, DontAlignCols, ",", "\n", "", "", "", "\n"));
+	csv.close();
+}
 
 int main() {
 	const auto &f = [](double x, double y, double z)->double {
-		return cos(x)+cos(y)+cos(z);
+		return 10 * (cos(x)*sin(y) + cos(y)*sin(z) + cos(z)*sin(x))
+			- 0.5*(cos(2 * x)*cos(2 * y) + cos(2 * y)*cos(2 * z) + cos(2 * z)*cos(2 * x));
 	};
 	
-	std::vector<double>Vmin = { 0,0,0 };
-	std::vector<double>Vmax = { 2*PI,2*PI,2*PI };
-	double levelset = 0.5;
+	double x = 12.5, y = 2.5, z = 17.5;
+	std::vector<double>Vmin = { (x - 10)*0.1*PI,(y - 10)*0.1*PI,(z - 10)*0.1*PI };
+	std::vector<double>Vmax = { (x + 10)*0.1*PI,(y + 10)*0.1*PI,(z + 10)*0.1*PI };
+	double levelset = 5;
 
 	tpmsgen::TPMSGenerator<double,double> tg(f,Vmin, Vmax, 100);
-	Eigen::MatrixXd grid, value;
-	tg.getDistanceField(grid, value);
-	Eigen::MatrixXd V; Eigen::MatrixXi F;
-	tg.makeLevelSet(levelset, V, F);
-	writeOBJ("D:/project/tpmsGen/tpmsGen/data/p.obj", V, F);
+	Eigen::MatrixXd C2W(290, 2);
+	for (int i = 0; i <290; i++) {
+	std::vector<double> h = {1};
+	//for (int k = 0; k < h.size(); k++) {
+		std::vector<double> stretch = { 10 * h[0],10 * h[0],10*h[0] };
+		double density = 0;
+		Eigen::MatrixXd V; Eigen::MatrixXi F;
+		tg.makeLevelSet(0x3F, static_cast<double>(14.6-0.1*i), V, F, density);
+		C2W.row(i) << static_cast<double>(14.6 - 0.1*i),density;
+		std::cout << C2W.row(i) << std::endl;
+		//for (int j = 0; j < 3; j++) V.col(j) *= stretch[j] / (2 * PI);
+
+		/*writeOBJ("../data/TEST/TG_level_" + std::to_string(i) + "_density_" + std::to_string(density) + "_X_" + std::to_string(stretch[0])
+			+ "_Y_" + std::to_string(stretch[1]) + "_Z_" + std::to_string(stretch[2]) + ".obj", V, F);*/
+		//writeOBJ("../data/TEST/TG_level_" + std::to_string(i) + "_density_" + std::to_string(density) + "_X_" + std::to_string(1)
+			//+ "_Y_" + std::to_string(1) + "_Z_" + std::to_string(1) + ".obj", V, F);
+		//}
+		
+	}
+	writeLOG("../data/levelset2density.csv", C2W);
 	system("pause");
+	
 }
